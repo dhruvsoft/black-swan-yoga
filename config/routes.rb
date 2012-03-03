00f6007f3ab24aa8cd71584011918d09045779e8
Steven Zeiler <zeiler.steven@gmail.com>
2012-03-03 17:09:16 -0600

    removed index.html and adding default route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Store::Application.routes.draw do
+  root :to => 'yoga_sessions#index'
   resources :yoga_sessions
 
   resources :products
@@ -52,7 +53,7 @@ Store::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
